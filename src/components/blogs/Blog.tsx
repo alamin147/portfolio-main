10960568f3820639835660769b2fc545e02593bf
alamin.147 <alamin.14780@gmail.com>
2024-07-05 19:23:11 +0600

    update

--- src/components/blogs/Blog.tsx
+++ src/components/blogs/Blog.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Fade } from "react-awesome-reveal";
 import { BiMessageRoundedDetail } from "react-icons/bi";
 import { FaRegClock } from "react-icons/fa6";
 const Blog = () => {
@@ -28,41 +29,46 @@ const Blog = () => {
         <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 ">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mb-16">
             {/* card */}
-            {allBlog?.map((singleBlog: any) => {
-              return (
-                <div
-                  key={singleBlog?._id}
-                  className="rounded overflow-hidden shadow-lg flex flex-col"
-                >
-                  <div className="relative">
-                    <img className="w-full max-h-52" src={singleBlog?.imgUrl} />
-                    <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
+            <Fade cascade damping={0.1}>
+              {allBlog?.map((singleBlog: any) => {
+                return (
+                  <div
+                    key={singleBlog?._id}
+                    className="rounded overflow-hidden shadow-lg flex flex-col"
+                  >
+                    <div className="relative">
+                      <img
+                        className="w-full max-h-52"
+                        src={singleBlog?.imgUrl}
+                      />
+                      <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
 
-                    <div className="bg-gradient-to-r from-indigo-950 to-indigo-800 border-0 py-3 px-5 font-bold hover:bg-gradient-to-r hover:from-indigo-800 hover:to-indigo-950 transition duration-200 ease-in-out rounded-full text-xs absolute top-0 left-0 mt-3 ms-2 text-white">
-                      {singleBlog?.category}
+                      <div className="bg-gradient-to-r from-indigo-950 to-indigo-800 border-0 py-3 px-5 font-bold hover:bg-gradient-to-r hover:from-indigo-800 hover:to-indigo-950 transition duration-200 ease-in-out rounded-full text-xs absolute top-0 left-0 mt-3 ms-2 text-white">
+                        {singleBlog?.category}
+                      </div>
+                    </div>
+                    <div className="px-6 py-4 mb-auto bg-gradient-to-r from-indigo-950 to-indigo-800 text-white">
+                      <p className="font-medium text-lg  inline-block mb-2">
+                        {singleBlog?.title}
+                      </p>
+                      <p className="text-white text-sm">{singleBlog?.des}</p>
+                    </div>
+
+                    <div className="px-6 py-3 flex flex-row items-center justify-between text-white bg-gradient-to-r from-indigo-950 to-indigo-800">
+                      <span className="py-1 text-xs font-regular  mr-1 flex flex-row items-center">
+                        <FaRegClock size="12" />
+                        <span className="ml-1">6 mins ago</span>
+                      </span>
+
+                      <span className="py-1 text-xs font-regular  mr-1 flex flex-row items-center ">
+                        <BiMessageRoundedDetail size="15" />
+                        <span className="ml-1">39 Comments</span>
+                      </span>
                     </div>
                   </div>
-                  <div className="px-6 py-4 mb-auto bg-gradient-to-r from-indigo-950 to-indigo-800 text-white">
-                    <p className="font-medium text-lg  inline-block mb-2">
-                      {singleBlog?.title}
-                    </p>
-                    <p className="text-white text-sm">{singleBlog?.des}</p>
-                  </div>
-
-                  <div className="px-6 py-3 flex flex-row items-center justify-between text-white bg-gradient-to-r from-indigo-950 to-indigo-800">
-                    <span className="py-1 text-xs font-regular  mr-1 flex flex-row items-center">
-                      <FaRegClock size="12" />
-                      <span className="ml-1">6 mins ago</span>
-                    </span>
-
-                    <span className="py-1 text-xs font-regular  mr-1 flex flex-row items-center ">
-                      <BiMessageRoundedDetail size="15" />
-                      <span className="ml-1">39 Comments</span>
-                    </span>
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </Fade>
           </div>
         </div>
       </div>
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Buttons from "../button/Buttons";
 import "./project.css";
 import { InputFields } from "../../pages/projectPage/ProjectsPage";
+import { Fade } from "react-awesome-reveal";
 
 const Projects = () => {
   const [allProject, setAllProject] = useState<InputFields[]>();
@@ -28,28 +29,31 @@ const Projects = () => {
       <div className="grid-cols-1 sm:grid md:grid-cols-3 gap-4 mb-20">
         {/* /////////////////////////////////////////////////// */}
 
-        {allProject?.map((singleProject: InputFields) => {
-          return (
-            <div
-              key={singleProject?._id}
-              className="relative mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0 group"
-            >
-              <a href="#!">
-                <img
-                  className="rounded-lg "
-                  src={singleProject?.imgUrl}
-                  alt="Hollywood Sign on The Hill"
-                />
-              </a>
-              <div className="hover:shadow-lg hover:shadow-indigo-600/60 rounded-lg  absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ">
-                <h1 className="font-semibold mb-3">{singleProject?.title}</h1>_
-                <a href={`/projects/${singleProject?._id}`}>
-                  <Buttons text="View Details" />
+        <Fade cascade damping={0.2}>
+          {allProject?.map((singleProject: InputFields) => {
+            return (
+              <div
+                key={singleProject?._id}
+                className="relative mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0 group"
+              >
+                <a href="#!">
+                  <img
+                    className="rounded-lg "
+                    src={singleProject?.imgUrl}
+                    alt="Hollywood Sign on The Hill"
+                  />
                 </a>
+                <div className="hover:shadow-lg hover:shadow-indigo-600/60 rounded-lg  absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 ">
+                  <h1 className="font-semibold mb-3">{singleProject?.title}</h1>
+                  _
+                  <a href={`/projects/${singleProject?._id}`}>
+                    <Buttons text="View Details" />
+                  </a>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </Fade>
       </div>
     </div>
   );
