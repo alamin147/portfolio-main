15f7a9b545f650cb913de620a5ec9ceb6399292c
alamin147 <alamin.14780@gmail.com>
2025-05-27 00:37:45 +0600

    main

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import router from './routes/index.tsx'
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
